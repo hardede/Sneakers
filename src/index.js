f4009396043f9e1d9619e8e05debe9863e33d5ea
hardede <dimakostyuk50@gmail.com>
2022-03-02 21:03:20 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import "macro-css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 
   document.getElementById("root")
 );
